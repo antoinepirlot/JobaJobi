91589cd72d2a948a8c6b9bd68c1de2c1ee16b650
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-11-12 12:50:27 +0100

    [ADD] component for input in form

---
+++ src/components/InputInForm.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  labelName: {
+    type: String,
+    required: true,
+  },
+  typeInput: {
+    type: String,
+    required: true,
+  }
+});
+</script>
+
+<template>
+  <div>
+    <label v-bind:for="name">{{labelName}}: </label>
+    <input v-bind:type="typeInput" v-bind:name="name" v-bind:id="name" required />
+  </div>
+</template>
