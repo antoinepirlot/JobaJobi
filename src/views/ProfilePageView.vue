476f3b02852b48193ec5c82253ff397b2bcba78a
dimitriadisnicolas <nicolas.dimitriadis@student.vinci.be>
2022-11-12 15:31:08 +0100

    [ADD] : add profile page display

--- src/views/ProfilePageView.vue
+++ src/views/ProfilePageView.vue
@@ -1,10 +1,44 @@
 <script setup>
-import { ref } from "vue";
-const titleClass = ref("Profile");
+import { ref, reactive } from "vue";
+
+//Temporary user object
+const user = reactive({
+  name: "IPL",
+  state: "Belgium",
+  phone_number: "0445789865",
+  type: "company",
+  description: "We are a belgium company",
+});
+//Retrieve session data
+//TODO
+//Check if the user is a Company or an Individual
 </script>
 
 <template>
-  <h1>{{ titleClass }}</h1>
+  <!-- Company View -->
+  <template v-if="user.type === 'company'">
+    <div>
+      <h1>Profil de l'entreprise</h1>
+      <div><span>Nom de l'entreprise :</span> {{ user.name }}</div>
+      <div><span>Ville de l'entreprise:</span> {{ user.state }}</div>
+      <div><span>Numéro de téléphone : </span>{{ user.phone_number }}</div>
+      <div><span>Description :</span> {{ user.description }}</div>
+    </div>
+  </template>
+
+  <!-- Individual View -->
+  <template v-else-if="user.type === 'individual'">
+    <div>
+      <h1>Profil de l'entreprise</h1>
+      <div><span>Nom :</span> {{ user.lastname }}</div>
+      <div><span>Prénom :</span> {{ user.name }}</div>
+      <div><span> Numéro de téléphone :</span> {{ user.phone_number }}</div>
+    </div>
+  </template>
 </template>
 
-<style></style>
+<style>
+span {
+  font-weight: bold;
+}
+</style>
