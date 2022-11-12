4915bba115ff4ac074340b7f03efd005030fec2b
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-11-12 12:52:54 +0100

    [ADD] component for select in form

---
+++ src/components/SelectInForm.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <label v-bind:for="name">Type de contrat:</label>
+    <select v-bind:name="name">
+      <option v-for="op in options">
+        {{ op }}
+      </option>
+    </select>
+  </div>
+</template>
