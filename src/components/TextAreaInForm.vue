bab14326737a0df33fbfcc9952b634024ea1482f
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-11-12 13:33:12 +0100

    [ADD] component for textarea in form

---
+++ src/components/TextAreaInForm.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  labelName: {
+    type: String,
+    required: true,
+  },
+  rows: {
+    type: String,
+  },
+  cols: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <label v-bind:for="name" class="leftLabel">{{ labelName }}:</label>
+    <textarea
+      v-bind:name="name"
+      v-bind:id="name"
+      v-bind:rows="rows"
+      v-bind:cols="cols"
+      required
+    >
+    </textarea>
+  </div>
+</template>
+
+<style>
+.leftLabel {
+  vertical-align: top;
+}
+</style>
