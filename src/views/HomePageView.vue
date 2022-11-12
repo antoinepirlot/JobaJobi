d5035adf19afc207471085722949c50ce21fa4aa
dimitriadisnicolas <nicolas.dimitriadis@student.vinci.be>
2022-11-12 17:00:16 +0100

    [ADD] : add display logic of the offers cards

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -2,10 +2,50 @@
 import { ref } from "vue";
 
 const titleClass = ref("Toutes les offres");
+
+//Get all offers
+const offers = [
+  {
+    id: 1,
+    title: "Job d'été",
+    email: "ipl@vinci.be",
+    type: "CDI",
+    description: "Un super job dans la forêt magique",
+    date: Date.now(),
+    company: "IPL",
+    city: "Bruxelles",
+  },
+  {
+    id: 2,
+    title: "Job étudiant",
+    email: "embie@embie.be",
+    type: "CDD",
+    description: "Un super job étudiant",
+    date: Date.now(),
+    company: "Embie",
+    city: "Bruxelles",
+  },
+];
 </script>
 
 <template>
-  <h1>{{ titleClass }}</h1>
+  <div class="homepage-display">
+    <h1>{{ titleClass }}</h1>
+    <div class="cards-offers">
+      <div v-for="offer in offers" :key="offer.id">
+        <p>{{ offer.title }}, {{ offer.company }}, {{ offer.city }}</p>
+        <p></p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style>
+.homepage-display {
+  text-align: center;
+}
+
+.cards-offers {
+  text-align: left;
+}
+</style>
