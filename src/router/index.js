74cc7c47a3ae7f7af28158d315bcc7e17ddefbe5
dimitriadisnicolas <nicolas.dimitriadis@student.vinci.be>
2022-11-12 12:42:57 +0100

    [ADD] : init the profile page view

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import CreateJobOfferView from '../views/CreateJobOfferView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import CreateJobOfferView from "../views/CreateJobOfferView.vue";
+import ProfilePageView from "../views/ProfilePageView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/createJobOffer',
-      name: 'createJobOffer',
-      component: CreateJobOfferView
-    }
-  ]
-})
+      path: "/createJobOffer",
+      name: "createJobOffer",
+      component: CreateJobOfferView,
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: ProfilePageView,
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/ProfilePageView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+const titleClass = ref("Profile");
+</script>
+
+<template>
+  <h1>{{ titleClass }}</h1>
+</template>
+
+<style></style>
