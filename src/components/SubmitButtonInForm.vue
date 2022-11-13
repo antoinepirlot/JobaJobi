ef2c137c9cd169d2db28b59c1591b946c2ab6055
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-11-13 13:53:12 +0100

    [ADD] component for submit button in form

---
+++ src/components/SubmitButtonInForm.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+    <div>
+      <button type="submit">{{name}}</button>
+    </div>
+</template>
