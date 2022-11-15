45134cd689ea96990d566800c9e678fb13b80fb8
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-11-15 12:38:29 +0100

    Update: redirect unknown page to the home page

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/:catchAll(.*)",
+      name: "not found",
+      component: HomeView
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView
