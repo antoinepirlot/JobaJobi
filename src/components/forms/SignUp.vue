343baa8e6f5a7a518d4b32fcbc23e24ae5250401
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-11-18 12:24:28 +0100

    Update: signup by sending data to api

--- src/components/forms/SignUp.vue
+++ src/components/forms/SignUp.vue
@@ -1,9 +1,10 @@
-<script setup>
+<script async setup>
 import {ref} from "vue";
 import InputInForm from "@/components/InputInForm.vue";
 import router from "@/router";
 import ErrorMessage from "@/components/ErrorMessage.vue";
 import utils from "@/utils/utils";
+import api_requests from "@/utils/api_requests";
 
 const options = ["Particulier", "Entreprise"];
 const selectedOption = ref(options[0]);
@@ -23,45 +24,59 @@ function goToConnectionPage() {
   router.push("/login")
 }
 
-function signup(e) {
+async function signup(e) {
   e.preventDefault();
   errorMessage.value = "";
-  if(lastName.value === "") {
+  if (lastName.value === "") {
     errorMessage.value = "Missing lastname";
     return;
   }
-  if(firstName.value === "") {
+  if (firstName.value === "") {
     errorMessage.value = "Missing firstName";
     return;
   }
-  if(birthday.value === null) {
+  if (birthday.value === null) {
     errorMessage.value = "Missing birthday";
     return;
   }
-  if(phone.value === "") {
+  if (phone.value === "") {
     errorMessage.value = "Missing phone";
     return;
   }
-  if(email.value === "") {
+  if (email.value === "") {
     errorMessage.value = "Missing email";
     return;
   }
-  if(password.value === "") {
+  if (password.value === "") {
     errorMessage.value = "Missing password";
     return;
   }
-  if(confirmPassword.value === "") {
+  if (confirmPassword.value === "") {
     errorMessage.value = "Missing Confirmation Password";
     return;
   }
-  if(type.value === "") {
+  if (type.value === "") {
     errorMessage.value = "Missing Type";
     return;
   }
-  if(!utils.checkPassword(password, confirmPassword)) {
+  if (!utils.checkPassword(password, confirmPassword)) {
     errorMessage.value = "Passwords don't match";
   }
-
+  const user = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    birthday: birthday.value,
+    phone: phone.value,
+    email: email.value,
+    password: password.value,
+    type: type.value
+  }
+  try {
+    await api_requests.signUp(user);
+  } catch (e) {
+    console.error(e.message);
+  }
+  goToConnectionPage();
 }
 </script>
 
@@ -99,13 +114,15 @@ function signup(e) {
     <input @click="signup" id="submit_signup" type="submit" value="S'inscrire">
     <ErrorMessage v-if="errorMessage !== ''" :error-message="errorMessage"/>
   </form>
-  <p>Déjà un compte? <button @click="goToConnectionPage">Connexion</button></p>
+  <p>Déjà un compte?
+    <button @click="goToConnectionPage">Connexion</button>
+  </p>
 </template>
 
 <style>
-  #submit_signup {
-    background-color: blue;
-    color: white;
-    border-radius: 30px;
-  }
+#submit_signup {
+  background-color: blue;
+  color: white;
+  border-radius: 30px;
+}
 </style>
