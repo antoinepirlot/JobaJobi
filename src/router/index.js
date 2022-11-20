9d2ee24037da61e8521a9b755d6b15a22a584943
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-11-20 21:21:55 +0100

    Add: favorite view

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HomePageView from "../views/HomePageView.vue";
 import LoginView from "../views/LoginView.vue";
 import SignUpView from "../views/SignUpView.vue";
 import LogoutView from '../views/LogoutView.vue';
+import FavoritesView from "@/views/FavoritesView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -45,6 +46,11 @@ const router = createRouter({
       path: "/logout",
       name: "logout",
       component: LogoutView
+    },
+    {
+      path: "/favorites",
+      name: "favorites",
+      component: FavoritesView
     }
   ],
 });
---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref} from "vue";
+import router from "@/router";
+import utils from "@/utils/utils";
+import api_requests from "@/utils/api_requests";
+
+// if(!utils.isConnected()) {
+//   router.push("/login")
+// }
+
+const userId = utils.getItem("user").id;
+const tempFav = api_requests.getFavorites();
+const favorites = ref([]);
+
+</script>
+
+<template>
+  <h1>Vos offres d'emplois favorites</h1>
+</template>
