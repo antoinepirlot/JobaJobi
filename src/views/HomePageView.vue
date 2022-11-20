4f20309a187d6d5ce375008e3673b69d7adb6234
dimitriadisnicolas <nicolas.dimitriadis@student.vinci.be>
2022-11-20 12:56:51 +0100

    [ADD] :  add backend request for add intrested user

--- src/views/HomePageView.vue
+++ src/views/HomePageView.vue
@@ -1,31 +1,120 @@
 <script setup>
+import OfferCardVue from "../components/OfferCard.vue";
 import { ref } from "vue";
+import router from "@/router";
 
 const titleClass = ref("Toutes les offres");
 
+const offers = ref([]);
+const user = ref([]);
+
+//Click on view more button --> go to JobOfferDetailsView
+const onButtonClick = (id) => {
+  router.push(`/jobOfferDetails?id=${id}`);
+};
+
+//Click on intrested button --> add intrest to an offer
+const onIntrestedClick = (idOffer, idUser) => {
+  // Backend Request /api/jobOffers/createIntrested
+  //add authorize
+
+  const addIntrestedToBackend = async (idOffer, idUser) => {
+    try {
+      const body = {
+        idOffer: idOffer,
+        idUser: idUser,
+      };
+      const options = {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          //TODO : add authorization
+        },
+      };
+      const response = await fetch("/api/jobOffers/createIntrested", options);
+      if (!response.ok) {
+        throw new Error(
+          "fetch error : " + response.status + " : " + response.statusText
+        );
+      }
+      return await response.json();
+    } catch (err) {
+      console.error("error: ", err);
+    }
+  };
+  addIntrestedToBackend(idOffer, idUser);
+};
+
+//Get User Session
+const getUserFromSession = async () => {
+  //Set the header of the request
+  const header = new Headers();
+  header.append("Content-Type", "application/json");
+  header.append(
+    "Authorization",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZhY2Vib2tAZ21haWwuY29tIiwiaWF0IjoxNjY4Nzg0Mzg3LCJleHAiOjE3NTUxODQzODd9.39V_8TOGeop3GMDPqOrDVo431iyFkwdxMwFmIVcKrcA"
+  );
+  //Set the options of the request
+  const options = {
+    method: "GET",
+    headers: header,
+  };
+  await fetch("/api/users/getUserSession", options)
+    .then(async (response) => {
+      const data = await response.json();
+      // check for error response
+      if (!response.ok) {
+        // get error message from body or default to response statusText
+        throw new Error(
+          "fetch error : " + response.status + " : " + response.statusText
+        );
+      }
+      //Assign the data to the user ref
+      user.value = data;
+
+      console.log(user.value);
+    })
+    .catch((error) => {
+      console.error("There was an error : ", error);
+    });
+};
+
+getUserFromSession();
+
 //Get all offers
-const offers = [
-  {
-    id: 1,
-    title: "Job d'été",
-    email: "ipl@vinci.be",
-    type: "CDI",
-    description: "Un super job dans la forêt magique",
-    date: Date.now(),
-    company: "IPL",
-    city: "Bruxelles",
-  },
-  {
-    id: 2,
-    title: "Job étudiant",
-    email: "embie@embie.be",
-    type: "CDD",
-    description: "Un super job étudiant",
-    date: Date.now(),
-    company: "Embie",
-    city: "Bruxelles",
-  },
-];
+const getAllOffers = async () => {
+  //Set the header of the request
+  const header = new Headers();
+  header.append("Content-Type", "application/json");
+  header.append(
+    "Authorization",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZhY2Vib2tAZ21haWwuY29tIiwiaWF0IjoxNjY4ODc3MDAwLCJleHAiOjE3NTUyNzcwMDB9.S0yeXyvlfJLVO7oDWDPNgv_JS3PS-YdhJjCTGe3tbdw"
+  );
+  //Set the options of the request
+  const options = {
+    method: "GET",
+    headers: header,
+  };
+  await fetch("/api/jobOffers/getAll", options)
+    .then(async (response) => {
+      const data = await response.json();
+      // check for error response
+      if (!response.ok) {
+        // get error message from body or default to response statusText
+        throw new Error(
+          "fetch error : " + response.status + " : " + response.statusText
+        );
+      }
+      //Assign the data to the user ref
+      offers.value = data;
+    })
+    .catch((error) => {
+      console.error("There was an error : ", error);
+    });
+};
+const isIntrested = () => {};
+getAllOffers();
 </script>
 
 <template>
@@ -33,8 +122,14 @@ const offers = [
     <h1>{{ titleClass }}</h1>
     <div class="cards-offers">
       <div v-for="offer in offers" :key="offer.id">
-        <p>{{ offer.title }}, {{ offer.company }}, {{ offer.city }}</p>
-        <p></p>
+        <OfferCardVue
+          :offertitle="offer.title"
+          :offer-description="offer.description"
+          :offer-type="offer.contractType"
+          :id="offer.idJobOffer"
+          @on-button-click="onButtonClick($event)"
+          @on-intrested-click="onIntrestedClick($event, user.idUser)"
+        ></OfferCardVue>
       </div>
     </div>
   </div>
@@ -46,6 +141,6 @@ const offers = [
 }
 
 .cards-offers {
-  text-align: left;
+  text-align: center;
 }
 </style>
