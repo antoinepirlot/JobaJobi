2eed7b17590ea0418a49e72d2d3a7cd2ae1f9c70
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-11-22 13:00:55 +0100

    [ADD] add info of the company

--- src/views/JobOfferDetailsView.vue
+++ src/views/JobOfferDetailsView.vue
@@ -5,28 +5,23 @@ import router from "../router/index.js";
 import api_requests from "@/utils/api_requests";
 
 const offer = await api_requests.getJobOfferById(router.currentRoute.value.params.id);
-console.log(offer)
-
+const companyOfTheOffer = await api_requests.getUserById(offer.idCompany);
 const userConnected = await api_requests.getUserByToken();
-console.log(userConnected);
 const isCompanyOwnerOfJobOffer = userConnected.id === offer.idCompany; 
 let allInterested = [];
 if(isCompanyOwnerOfJobOffer){
   allInterested = await api_requests.getInterestedByIdJobOffer(router.currentRoute.value.params.id);
-  console.log(allInterested)
 }
-
 </script>
 
 <template>
     <h1>{{offer.title}}</h1>
     <h4>Date de publication : {{offer.publicationDate}}</h4>
     <h4>Description de l'entreprise : </h4>
-    <p>Insérer paragraphe entreprise</p> <!-- TODO -->
-    <p>Insérer infos entreprise</p> <!-- TODO -->
+    <p>Nom de l'entreprise : {{companyOfTheOffer.companyName}}</p>
+    <p>{{companyOfTheOffer.companyDescription}}</p>
     <h4>Description de l'offre : </h4>
     <p>{{offer.description}}</p>
     <h4>Mail de contact : {{offer.contactMail}}</h4>
-    <!-- TODO : add button to mark interest-->
     <ListInterested v-if="isCompanyOwnerOfJobOffer" v-bind:allInterested="allInterested"/>
 </template>
